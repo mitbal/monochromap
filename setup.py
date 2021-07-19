2cf94224c833bfcc2b31ef2346966b310305e22f
M Iqbal Tawakal <mit.iqi@gmail.com>
2021-07-19 18:01:59 +0700

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='monochromap',
     packages=['monochromap'],
-    version='0.1.0',
+    version='0.2.0',
     description='A highly opinionated way to paint and plot black and white map',
     long_description=long_description,
     long_description_content_type='text/markdown',
